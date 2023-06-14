cbd206e22e9179329fa2e0f3e96b3efd7da9e7a5
Egg Oxygen <ken442266@gmail.com>
2023-06-14 20:51:36 +0800

    fix: disable bootJar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 import org.jetbrains.kotlin.gradle.plugin.getKotlinPluginVersion
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
     alias(libs.plugins.jvm)
@@ -23,6 +24,10 @@ java {
 
 tasks.named<Jar>("javadocJar") {
     from(tasks.named("dokkaJavadoc"))
+}
+
+tasks.named<BootJar>("bootJar") {
+    enabled = false
 }
 
 tasks.withType<KotlinCompile> {
